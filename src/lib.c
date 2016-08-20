02d04b292effcb1e22dfbd3668643d0d0741de53
dirk1980ac <dirk1980ac@bf39286c-cf35-4af5-8e44-0950e6e02065>
2016-08-20 21:25:48 +0000

    Translated comments to englisch and some corrections done
    git-svn-id: svn+ssh://svn.code.sf.net/p/socketlayer/code/trunk@6 bf39286c-cf35-4af5-8e44-0950e6e02065

--- src/lib.c
+++ src/lib.c
@@ -54,8 +54,7 @@ void bindSocket(socket_t *sock, unsigned long adress, unsigned short port) {
 		errorExit("Couldn't bind to socket!");
 }
 
-/* Teile dem Socket mit, dass Verbindungswünsche
- * von Clients entgegengenommen werden. */
+/* Tells the socket to accept connections. */
 void listenSocket( socket_t *sock) {
 	if (listen(*sock, 5) == -1)
 		errorExit("Listen error!");
@@ -73,7 +72,7 @@ void acceptSocket( socket_t *socket, socket_t *new_socket) {
 		errorExit("Error on accept()!");
 }
 
-/* Baut die Verbindung zum Server auf. */
+/* Connects socket. */
 void connectSocket( socket_t *sock, char *serv_addr, unsigned short port) {
 	struct sockaddr_in server;
 	struct hostent *host_info;
@@ -81,53 +80,51 @@ void connectSocket( socket_t *sock, char *serv_addr, unsigned short port) {
 
 	memset(&server, 0, sizeof(server));
 	if ((addr = inet_addr(serv_addr)) != INADDR_NONE) {
-		/* argv[1] ist eine numerische IP-Adresse. */
 		memcpy((char *) &server.sin_addr, &addr, sizeof(addr));
 	} else {
-		/* Für den Fall der Fälle: Wandle den
-		 * Servernamen bspw. "localhost" in eine IP-Adresse um. */
+		/* Resolve hostname */
 		host_info = gethostbyname(serv_addr);
 		if (NULL == host_info)
-			errorExit("Unbekannter Server");
+			errorExit("Unknown server");
 		memcpy((char *) &server.sin_addr, host_info->h_addr,
 				host_info->h_length);
 	}
 	server.sin_family = AF_INET;
 	server.sin_port = htons(port);
 
-	/* Baue die Verbindung zum Server auf. */
+	/* Establish connection. */
 	if (connect(*sock, (struct sockaddr*) &server, sizeof(server)) < 0)
-		errorExit("Kann keine Verbindung zum Server herstellen");
+		errorExit("Unable to connect to server");
 }
 
-/* Daten versenden via TCP */
+/* Send data via TCP */
 void tcpSend( socket_t *sock, char *data, size_t size) {
 	if (send(*sock, data, size, 0) == SOCKET_ERROR)
-		errorExit("Fehler bei send()");
+		errorExit("ERROR: send()");
 }
 
-/* Daten empfangen via TCP */
+/* Receive data via TCP */
 int tcpRecv( socket_t *sock, char *data, size_t size) {
 	int len;
 	len = recv(*sock, data, size, 0);
 	if (len > 0 || len != SOCKET_ERROR)
 		data[len] = '\0';
 	else
-		errorExit("Fehler bei recv()");
+		errorExit("ERROR: recv()");
 	return len;
 }
 
-/* Daten senden via UDP */
+/* Send data via UDP */
 void udpSend( socket_t *sock, char *data, size_t size, char *addr,
 		unsigned short port) {
 	struct sockaddr_in addr_sento;
 	struct hostent *h;
 	int rc;
 
-	/* IP-Adresse vom Server überprüfen */
+	/* Resolve hostname */
 	h = gethostbyname(addr);
 	if (h == NULL)
-		errorExit("Unbekannter Host?");
+		errorExit("Unknown host?");
 
 	addr_sento.sin_family = h->h_addrtype;
 	memcpy((char *) &addr_sento.sin_addr.s_addr, h->h_addr_list[0],
@@ -137,10 +134,10 @@ void udpSend( socket_t *sock, char *data, size_t size, char *addr,
 	rc = sendto(*sock, data, size, 0, (struct sockaddr *) &addr_sento,
 			sizeof(addr_sento));
 	if (rc == SOCKET_ERROR)
-		errorExit("Konnte Daten nicht senden - sendto()");
+		errorExit("Could not send data - sendto()");
 }
 
-/* Daten empfangen via UDP */
+/* Receive data via UDP */
 int udpRecv( socket_t *sock, void *data, size_t size) {
 	struct sockaddr_in addr_recvfrom;
 	unsigned int len;
@@ -150,12 +147,12 @@ int udpRecv( socket_t *sock, void *data, size_t size) {
 	n = recvfrom(*sock, data, size, 0, (struct sockaddr *) &addr_recvfrom,
 			&len);
 	if (n == SOCKET_ERROR)
-		errorExit("Fehler bei recvfrom()");
+		errorExit("ERROR: recvfrom()");
 
 	return n;
 }
 
-/* Socket schließen und Winsock freigeben */
+/* Close socket and release Winsock */
 void closeSocket( socket_t *sock) {
 	closesocket(*sock);
 }
@@ -163,34 +160,31 @@ void closeSocket( socket_t *sock) {
 void cleanUp(void) {
 	/* Cleanup Winsock */
 	WSACleanup();
-	printf("Aufraeuumarbeiten erledigt ...\n");
+	printf("Done cleanup...\n");
 }
 #else
 #include "socketlayer.h"
 
-/* Die Funktion gibt aufgetretene Fehler aus und
- * beendet die Anwendung. */
+/* Returns Errors and exits application. */
 void errorExit(char *error_message) {
 	fprintf(stderr, "%s: %s\n", error_message, strerror(errno));
 	exit(EXIT_FAILURE);
 }
 
+/* Create socket */
 int createSocket(int af, int type, int protocol) {
 	socket_t sock;
 	const int y = 1;
-	/* Erzeuge das Socket. */
 	sock = socket(af, type, protocol);
 	if (sock < 0)
-		errorExit("Fehler beim Anlegen eines Sockets");
+		errorExit("Unable to create socket");
 
-	/* Mehr dazu siehe Anmerkung am Ende des Listings ... */
 	setsockopt(sock, SOL_SOCKET,
 	SO_REUSEADDR, &y, sizeof(int));
 	return (sock);
 }
 
-/* Erzeugt die Bindung an die Serveradresse,
- * (genauer gesagt an einen bestimmten Port). */
+/* Binds the socket to address and Port. */
 void bindSocket(socket_t *sock, unsigned long adress, unsigned short port) {
 	struct sockaddr_in server;
 	memset(&server, 0, sizeof(server));
@@ -198,19 +192,17 @@ void bindSocket(socket_t *sock, unsigned long adress, unsigned short port) {
 	server.sin_addr.s_addr = htonl(adress);
 	server.sin_port = htons(port);
 	if (bind(*sock, (struct sockaddr*) &server, sizeof(server)) < 0)
-		errorExit("Kann das Socket nicht \"binden\"");
+		errorExit("Unable to bind socket");
 }
 
-/* Teile dem Socket mit, dass Verbindungswünsche
- * von Clients entgegengenommen werden. */
+/* Tells the socket to accept connections. */
 void listenSocket( socket_t *sock) {
 	if (listen(*sock, 5) == -1)
-		errorExit("Fehler bei listen");
+		errorExit("Error: listen");
 }
 
-/* Bearbeite die Verbindungswünsche von Clients.
- * Der Aufruf von accept() blockiert so lange,
- * bis ein Client Verbindung aufnimmt. */
+/* Handle connections to the clients.
+ * accept() blocks until client accepts connection. */
 void acceptSocket( socket_t *socket, socket_t *new_socket) {
 	struct sockaddr_in client;
 	unsigned int len;
@@ -218,10 +210,10 @@ void acceptSocket( socket_t *socket, socket_t *new_socket) {
 	len = sizeof(client);
 	*new_socket = accept(*socket, (struct sockaddr *) &client, &len);
 	if (*new_socket == -1)
-		errorExit("Fehler bei accept");
+		errorExit("Error in accept");
 }
 
-/* Baut die Verbindung zum Server auf. */
+/* Connects socket. */
 void connectSocket(socket_t *sock, char *serv_addr, unsigned short port) {
 	struct sockaddr_in server;
 	struct hostent *host_info;
@@ -229,31 +221,30 @@ void connectSocket(socket_t *sock, char *serv_addr, unsigned short port) {
 
 	memset(&server, 0, sizeof(server));
 	if ((addr = inet_addr(serv_addr)) != INADDR_NONE) {
-		/* argv[1] ist eine numerische IP-Adresse */
 		memcpy((char *) &server.sin_addr, &addr, sizeof(addr));
 	} else {
-		/* Für den Fall der Fälle: Wandle den
-		 * Servernamen bspw. "localhost" in eine IP-Adresse um. */
+		/* Resolve hostname */
 		host_info = gethostbyname(serv_addr);
 		if (NULL == host_info)
-			errorExit("Unbekannter Server");
+			errorExit("Unknown host");
 		memcpy((char *) &server.sin_addr, host_info->h_addr,
 		host_info->h_length);
 	}
 	server.sin_family = AF_INET;
 	server.sin_port = htons(port);
-	/* Baue die Verbindung zum Server auf. */
+
+	/* Establish connection */
 	if (connect(*sock, (struct sockaddr *) &server, sizeof(server)) < 0)
-		errorExit("Kann keine Verbindung zum Server herstellen");
+		errorExit("Could not connect to server");
 }
 
-/* Daten versenden via TCP */
+/* Send data via TCP */
 void tcpSend( socket_t *sock, char *data, size_t size) {
 	if (send(*sock, data, size, 0) == -1)
 		errorExit("Fehler bei send()");
 }
 
-/* Daten empfangen via TCP */
+/* Receive data via TCP */
 int tcpRecv( socket_t *sock, char *data, size_t size) {
 	unsigned int len;
 	len = recv(*sock, data, size, 0);
@@ -261,11 +252,11 @@ int tcpRecv( socket_t *sock, char *data, size_t size) {
 		data[len] = '\0';
 		return (len);
 	} else {
-		/* errorExit("Fehler bei recv()"); */
+		errorExit("ERROR on recv()"); */
 		return (-1);
 	}
 }
-/* Daten senden via UDP */
+/* Send data via UDP */
 void udpSend( socket_t *sock, char *data, size_t size, char *addr,
 		unsigned short port) {
 	struct sockaddr_in addr_sento;
@@ -275,7 +266,7 @@ void udpSend( socket_t *sock, char *data, size_t size, char *addr,
 	/* IP-Adresse des Servers überprüfen */
 	h = gethostbyname(addr);
 	if (h == NULL)
-		errorExit("Unbekannter Host?");
+		errorExit("Unknown host?");
 
 	addr_sento.sin_family = h->h_addrtype;
 	memcpy((char *) &addr_sento.sin_addr.s_addr, h->h_addr_list[0],
@@ -285,10 +276,10 @@ void udpSend( socket_t *sock, char *data, size_t size, char *addr,
 	rc = sendto(*sock, data, size, 0, (struct sockaddr *) &addr_sento,
 			sizeof(addr_sento));
 	if (rc < 0)
-		errorExit("Konnte Daten nicht senden - sendto()");
+		errorExit("Could not send - sendto()");
 }
 
-/* Daten empfangen via UDP */
+/* Recieve data via UDP */
 int udpRecv( socket_t *sock, void *data, size_t size) {
 	struct sockaddr_in addr_recvfrom;
 	unsigned int len;
@@ -298,20 +289,19 @@ int udpRecv( socket_t *sock, void *data, size_t size) {
 	n = recvfrom(*sock, data, size, 0, (struct sockaddr *) &addr_recvfrom,
 			&len);
 	if (n < 0) {
-		printf("Keine Daten empfangen ...\n");
+		printf("Nothing received...\n");
 		return (-1);
 	}
 	return (n);
 }
 
-/* Socket schließen */
+/* Close socket */
 void closeSocket( socket_t *sock) {
 	close(*sock);
 }
 
-/* Unter Linux/UNIX ist nichts zu tun ... */
+/* Empty under Linux... */
 void cleanUp(void) {
-	printf("Aufraeumarbeiten erledigt ...\n");
 	return;
 }
 #endif
--- src/socketlayer.h
+++ src/socketlayer.h
@@ -2,11 +2,11 @@
  ============================================================================
  Name        : socketlayer.h
  Author      : Dirk Gottschalk
- Version     : 1.1.3
+ Version     : 2.0.0
  Copyright   : (c) 2015 Dirk Gottschalk <dirk.gottschalk1980@googlemail.com>
  Description : Network Socket abstraction Layer for Windows / Linux
  ============================================================================
- */
+*/
 
 #ifndef SOCKETLAYER_H_
 #define SOCKETLAYER_H_
@@ -34,26 +34,26 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+#endif /* __MINGW32__*/
+
 /* Globally defined socket type */
 #define socket_t int
 
-#endif /* __MINGW32__*/
-
 /* Funktion prototypes */
-extern void errorExit(char *error_message);
-extern int createSocket( int af, int type, int protocol );
-extern void bindSocket(socket_t
-  *sock, unsigned long adress, unsigned short port);
-extern void listenSocket( socket_t *sock );
-extern void acceptSocket( socket_t *new_socket, socket_t *socket );
+extern void errorExit(char *error_message); /* Exit with error message */
+extern int createSocket( int af, int type, int protocol ); /* Create socket */
+extern void bindSocket(socket_t *sock, unsigned long adress,
+                       unsigned short port); /* Bind socket */
+extern void listenSocket( socket_t *sock ); /* Listen on socket */
+extern void acceptSocket( socket_t *new_socket, socket_t *socket ); /* Accept conn. */
 extern void connectSocket(socket_t *sock, char *serv_addr,
- unsigned short port);
-extern void tcpSend( socket_t *sock, char *data, size_t size);
-extern int tcpRecv( socket_t *sock, char *data, size_t size);
+                          unsigned short port); /* Connect socket */
+extern void tcpSend( socket_t *sock, char *data, size_t size); /* Send TCP */
+extern int tcpRecv( socket_t *sock, char *data, size_t size); /* Receive TCP */
 extern void udpSend ( socket_t *sock, char *data, size_t size,
-  char *addr, unsigned short port);
-extern int udpRecv( socket_t *sock, void *data, size_t size);
-extern void closeSocket( socket_t *sock );
-extern void cleanUp(void);
+                     char *addr, unsigned short port); /* Send UDP */
+extern int udpRecv( socket_t *sock, void *data, size_t size); /* Receive UDP */
+extern void closeSocket( socket_t *sock ); /* Close socket */
+extern void cleanUp(void); /* Do cleanup */
 
 #endif /* SOCKETLAYER_H_ */
