2b6b84c7d69ac21dc703e19b2f754b3cf2b63e39
dirk1980ac <dirk1980ac@bf39286c-cf35-4af5-8e44-0950e6e02065>
2017-01-28 01:30:13 +0000

    Reviewed code and some cleanup
    git-svn-id: svn+ssh://svn.code.sf.net/p/socketlayer/code/trunk@40 bf39286c-cf35-4af5-8e44-0950e6e02065

--- src/socketlayer.h
+++ src/socketlayer.h
@@ -4,9 +4,9 @@
  Author      : Dirk Gottschalk
  Version     : 2.0.0
  Copyright   : (c) 2015 Dirk Gottschalk <dirk.gottschalk1980@googlemail.com>
- Description : Header for socket abstraction layer
- ============================================================================
- */
+	 Description : Header for socket abstraction layer
+	 ============================================================================
+	 */
 
 #ifndef SOCKETLAYER_H_
 #define SOCKETLAYER_H_
@@ -41,14 +41,14 @@
 extern void SLLIB_errorExit (char *error_message); /* Exit with error message */
 extern int SLLIB_createSocket (int af, int type, int protocol); /* Create socket */
 extern void SLLIB_bindSocket (socket_t *sock, unsigned long adress,
-		unsigned short port); /* Bind socket */
+							  unsigned short port); /* Bind socket */
 extern void SLLIB_listenSocket ( socket_t *sock); /* Listen on socket */
 extern void SLLIB_acceptSocket ( socket_t *new_socket, socket_t *socket); /* Accept conn. */
 extern void SLLIB_connectSocket (socket_t *sock, char *serv_addr, unsigned short port); /* Connect socket */
 extern void SLLIB_tcpSend ( socket_t *sock, char *data, size_t size); /* Send TCP */
 extern int SLLIB_tcpRecv ( socket_t *sock, char *data, size_t size); /* Receive TCP */
 extern void SLLIB_udpSend ( socket_t *sock, char *data, size_t size, char *addr,
-		unsigned short port); /* Send UDP */
+						   unsigned short port); /* Send UDP */
 extern int SLLIB_udpRecv ( socket_t *sock, void *data, size_t size); /* Receive UDP */
 extern void SLLIB_closeSocket ( socket_t *sock); /* Close socket */
 extern void SLLIB_cleanUp (void); /* Do cleanup */
